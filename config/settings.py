5015e6cc8fdf9b3cda3bd647acf906692218dd40
Aliyev Temur Toxirjon o'g'li <aliyer.temur95@gmail.com>
2025-11-01 11:05:33 +0500

    req.txt alowwed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7isb-_ux#!y_e0ooyem@m2wiq*6*e#mwr3-_@rzw^_2r!xp06d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
